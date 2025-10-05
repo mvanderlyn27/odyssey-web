5706e6e421ff51d67b185eeadbdf52e6f0a5672a
michael <mvanderlyn27@gmail.com>
2025-10-05 01:37:56 -0400

    posthog fixes

--- src/components/StoreButtons.tsx
+++ src/components/StoreButtons.tsx
@@ -1,11 +1,16 @@
+import { usePostHog } from "posthog-js/react";
+
 export default function StoreButtons() {
+  const posthog = usePostHog();
+
   return (
     <div className="flex flex-row justify-center md:justify-start gap-4 my-6 w-full max-w-sm mx-auto md:mx-0">
       <a
         href={import.meta.env.VITE_APP_STORE_URL}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex-1 overflow-visible">
+        className="flex-1 overflow-visible"
+        onClick={() => posthog.capture("app_store_button_clicked")}>
         <img
           src="/app_store.png"
           alt="App Store"
@@ -16,7 +21,8 @@ export default function StoreButtons() {
         href={import.meta.env.VITE_PLAY_STORE_URL}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex-1 overflow-visible">
+        className="flex-1 overflow-visible"
+        onClick={() => posthog.capture("play_store_button_clicked")}>
         <img
           src="/play_store.png"
           alt="Google Play"
